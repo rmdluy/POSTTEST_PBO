99061c8772269e62ac3bc15bdf92e9a2014ff494
rmdluy <93176348+rmdluy@users.noreply.github.com>
2023-03-07 21:42:34 +0800

    Add files via upload

---
+++ 2109106011_ILHAM RAMADHAN_POSTTEST1/Barang.java
@@ -0,0 +1,18 @@
+public class Barang {
+   String nama;
+   int jumlah;
+   double berat;
+   String tujuan;
+
+   public Barang(String nama, int jumlah, double berat, String tujuan) {
+       this.nama = nama;
+       this.jumlah = jumlah;
+       this.berat = berat;
+       this.tujuan = tujuan;
+   }
+
+public void terupdate() {
+}
+
+   
+}
